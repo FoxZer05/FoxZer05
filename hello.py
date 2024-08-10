6281e45e1c21a3f1fcee23fcd1784e80a02dafc9
Justice Da Fox <166665982+FoxZer05@users.noreply.github.com>
2024-08-09 21:58:02 -0500

    My "Hello World" file
    The file was created in VS Code using Python (obviously XD). It is just useless code, mostly me messing around with the "print" function and assigning variables values.

---
+++ hello.py
@@ -0,0 +1,19 @@
+#This is the first line of code that I learned
+print("Hello World!")
+print("____________")
+#I added the second line afterwards just cuz :)
+
+#Now for a little tomfoolery
+print("Now I am just messing around with the 'print' input!")
+print("I like chicken alfredo!")
+#All that does is basically copy and paste whatever is in the string between the ""s in the ()s
+
+#I also learned that you can assign a value to basically any variable
+#For example
+net_worth = 500000
+bill = 10000
+
+print(net_worth - bill)
+#This just subtract 10000 from 500000 and spits out the total "490000"
+#Learned most of all this from a mix of "Khan Academy" and a "Python for Dummies"
+#What should I start to learn first so I can get into making games?
